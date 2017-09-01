42022117391857f49a32cd87498e2e23d09d80aa
Ali Emami <emamiali1993@gmail.com>
2017-09-01 15:21:26 -0700

    setup the reducers folder, actions folder, and added more dependencies

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+const Header = () => <p>Header</p>
+const Landing = () => <h1>This is the landing page</h1>
+const Dashboard = () => <h1>Dashboard</h1>
+const NewSpending = () => <h1>Form for new spending</h1>
+
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={Dashboard}/>
+            <Route exact path="/spending/new" component={NewSpending} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(null,actions)(App);
