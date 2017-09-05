bf11133b91266cde599a90ab4dd2aaff53b7f5fd
Ali Emami <emamiali1993@gmail.com>
2017-09-04 22:46:06 -0700

    added the routes and logic for bills

--- routes/billsRoutes.js
+++ routes/billsRoutes.js
@@ -1,16 +1,80 @@
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const requireLogin = require('../middlewares/requireLogin');
 
-const Bills = mongoose.model('bills');
+const Bill = mongoose.model('bills');
 
 
 module.exports = (app) => {
-  app.get('/api/bills', async (req, res) => {
-    const bills = await Bills.find({ _user: req.user.id });
-    console.log('this is bills: ', bills);
-    res.send({ message: "some message"})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+  app.get('/api/bills', function(req, res) {
+    Bill
+    .find({ _user: req.user_id })
+    .exec( function(err, bills) {
+      if (err || !bills ) {
+        return res.status(404).send({ message: 'Bills Not found!!' });
+      }
+      res.send(bills);
+    });
   });
-  app.post('/api/bill', async (req, res) => {
-    const newPost = new Post(req.body);
-      console.log('req from post: ', req);
+
+  //add the authentication middleware so the req will have access to the user. and the user ID.
+  app.post('/api/bills', requireLogin, function (req, res) {
+    const newBill = new Bill(req.body);
+    console.log(newBill);
+    console.log(req.user);
+    newBill.user = req.user_id;
+  });
+
+  app.get('/api/bills/:bill_id', function(req, res) {
+    Bill
+      .findById(req.params.bill_id)
+      .exec(function(err, foundBill) {
+        if (err || !foundBill) {
+          return res.status(404).send({ message: 'Bill Not found!!' });
+        }
+        res.send(foundBill);
+      });
+  });
+
+  app.put('/api/bills/:bill_id', function(req, res) {
+    let query = {
+      _id: req.params.bill_id
+    }
+
+    if (req.user_id) {
+      query.user = req.user_id
+    }
+
+    Bill
+      .findOneAndUpdate(query, req.body)
+      .exec(function(err, bill) {
+        if (err || !bill) {
+          return res.status(404).send({ message: 'Failed to update bill!!'});
+        }
+        res.status(204).send();
+      });
+  });
+
+  app.delete('/api/bills/:bill_id', function(req, res) {
+    let query = {
+      _id: req.params.bill_id
+    }
+
+    if (req.user_id) {
+      query.user = req.user_id
+    }
+
+    Bill
+      .findOneAndRemove(query)
+      .exec(function(err, bill) {
+        if (err || !bill) {
+          return res.status(404).send({ message: 'Failed to Delete Bill!!' });
+        }
+        res.status(204).send();
+      });
   });
 }
