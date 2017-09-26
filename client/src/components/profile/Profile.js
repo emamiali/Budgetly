d77d9a548231391967ff64fad6b6bd0fb2052c80
Ali Emami <emamiali1993@gmail.com>
2017-09-26 06:15:56 -0700

    code refactoring

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -21,7 +21,6 @@ class Profile extends Component {
 
   componentWillUpdate() {
     this.props.savingsIncome;
-    console.log(this.props);
   }
 
   showNewTotalFundsFrom(){
@@ -42,41 +41,45 @@ class Profile extends Component {
     var imageStr = auth.data.avatar;
     var newImageStr = imageStr.split("?sz=50")[0]+"?sz="+newAvatarSize;
 
-    return (
+    const noSavingsAndIncomeView =
       <div>
-        <div className="row">
-          <div className="col s6">
-            <img src={newImageStr} />
-            <p><strong>Name --- </strong>{auth.data.name}</p>
-            <p><strong>Email --- </strong>{auth.data.email}</p>
-          </div>
-          <div className="col s6">
-            { (!total || total.data.length === 0) ?
-              <div>
-                <div className="section"></div>
-                <div className="col s6">Please add an income and a savings goal</div>
-                <div className="section"></div>
-              </div> :
-              <div>
-                <p><strong>Savings --- </strong>  {total.data[0].savingsGoal}</p>
-                <p><strong>Total Earning --- {total.data[0].income}</strong>  </p>
-              </div>
-            }
-            { this.state.addNewSavingsAndIncome ?
-              <div>
-                <div className="section"></div>
-                <button
-                  onClick={() => this.showNewTotalFundsFrom()}
-                  className="waves-effect waves-light btn"
-                >
-                  Add Savings Goal and Income
-                </button>
-              </div>
-              : null }
+        <div className="section"></div>
+        <div className="col s6">Please add an income and a savings goal</div>
+      </div>;
 
-              { this.state.newTotalFundsFormVisible ? <NewTotalFunds /> : null }
-            </div>
+    if (!total) {
+      return <div>Loading ...</div>
+    }
 
+    const savingsAndIncomeView = (
+      <div>
+        <p><strong>Savings --- </strong>  {total.data[0].savingsGoal}</p>
+        <p><strong>Total Earning --- {total.data[0].income}</strong>  </p>
+      </div>
+    );
+
+    const addNewSavingsAndIncomeButton =
+      <div>
+        <div className="section"></div>
+        <button
+          onClick={() => this.showNewTotalFundsFrom()}
+          className="waves-effect waves-light btn"
+        >
+          Add Savings Goal and Income
+        </button>
+      </div>;
+
+    return (
+      <div className="row">
+        <div className="col s6">
+          <img src={newImageStr} />
+          <p><strong>Name --- </strong>{auth.data.name}</p>
+          <p><strong>Email --- </strong>{auth.data.email}</p>
+        </div>
+        <div className="col s6">
+          { (!total || total.data.length === 0) ? noSavingsAndIncomeView : savingsAndIncomeView }
+          { this.state.addNewSavingsAndIncome ? addNewSavingsAndIncomeButton :  null }
+          { this.state.newTotalFundsFormVisible ? <NewTotalFunds /> : null }
         </div>
       </div>
     );
