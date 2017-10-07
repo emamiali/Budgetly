fb059674bc9a26e99dfc73d6ff85a77cdf22ee56
Ali Emami <emamiali1993@gmail.com>
2017-10-07 09:23:23 -0700

    added a test pie chart from recharts library for D3

--- client/src/components/budget/BudgetRings.js
+++ client/src/components/budget/BudgetRings.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react';
+import { PieChart, Pie, Legend, Tooltip } from 'recharts';
 
-export default class BudgetRings extends Component {
+class BudgetRings extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      billDataArray : []
+    }
+  }
+
+  convertDataObjectToArray() {
+    const { billData } = this.props;
+
+    var billDataArray = Object.keys(billData).map( key => {
+      return billData[key];
+    });
+    this.setState={billDataArray: billDataArray}
+  }
+
   render() {
+    console.log(data);
     return (
-      <div className='row'>
-        <div className="col s4">
-          <p>Spending Ring</p>
+      <div className="row">
+        <div className="col s3">
+          <PieChart  width={800} height={400}>
+            <Pie dataKey='value' data={data}  cx={200} cy={200} innerRadius={70} outerRadius={90} label />
+            <Legend align="left"/>
+            <Tooltip />
+          </PieChart>
+
         </div>
-        <div className="col s4">
-          <p>Bill Ring</p>
+        <div className="col s3">
+          <PieChart  width={800} height={400}>
+            <Pie dataKey='value' data={data}  cx={200} cy={200} innerRadius={70} outerRadius={90} label />
+            <Legend align="left"/>
+            <Tooltip />
+          </PieChart>
+
         </div>
-        <div className="col s4">
-          <p>Saving Ring</p>
+        <div className="col s3">
+          <PieChart  width={800} height={400}>
+            <Pie dataKey='value' data={data}  cx={200} cy={200} innerRadius={70} outerRadius={90} label />
+            <Legend />
+            <Tooltip />
+          </PieChart>
+
         </div>
       </div>
     );
   };
 }
+
+export default BudgetRings;
+
+const data = [
+  {
+    name: "TotalSavings",
+    value: 2500,
+    fill: '#eb3939'
+  },
+  {
+    name: "SavingsRemaining",
+    value: 1000,
+    fill: 'rgb(15, 115, 232)'
+  }
+]
