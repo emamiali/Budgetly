3d47cce8186bbbf3fc10d5cdd10266f0dd369086
Ali Emami <emamiali1993@gmail.com>
2017-10-19 19:23:20 -0700

    moved all the dashboard logic to Dashboard Component from incomeComponent

--- client/src/components/budget/Dashboard.js
+++ client/src/components/budget/Dashboard.js
@@ -15,20 +15,9 @@ class Dashboard extends Component {
     this.props.fetchSavings();
   }
 
-  getTheTotalBill() {
-    const { bills } = this.props;
-
-    const billAmountArray = [];
-
-    const billObjectToIndividualBill = _.map(this.props.bills, bill => {
-      const IndividualBillAmount = bill.billAmount;
-      billAmountArray.push(IndividualBillAmount);});
-
-    const totalBill =
-      billAmountArray.reduce((a, b) => a + b, 0);
-  }
-
   render() {
+
+    //Total Bill Logic
     const { bills } = this.props;
 
     const billAmountArray = [];
@@ -39,13 +28,60 @@ class Dashboard extends Component {
 
     const totalBill =
       billAmountArray.reduce((a, b) => a + b, 0);
+
+
+    //Total Spending Logic
+    const { spendings } = this.props;
+
+    const spendingAmountArray = [];
+
+    const spendingObjectToIndividualSpending = _.map(this.props.spendings, spending => {
+      const IndividualSpendingAmount = spending.spendingAmount;
+      spendingAmountArray.push(IndividualSpendingAmount);
+    });
+
+    const totalSpending = spendingAmountArray.reduce((a, b) => a + b, 0);
+
+
+    //Total Savings Logic
+    const { savings } = this.props;
+
+    const savingsAmountArray = [];
+
+    const savingsObjectToIndividualSaving = _.map(this.props.savings, saving => {
+      const IndividualSavingAmount = saving.savingAmount;
+      savingsAmountArray.push(IndividualSavingAmount);
+    });
+
+    const totalSaving = savingsAmountArray.reduce((a, b) => a + b, 0);
+
+    //Income, Saving Goal, and Remaning Funds Logic
+    const { total } = this.props;
+    if (!total) {
+      return ( <div>Loading ...</div> )
+    }
+
+    if (!total.data[0]) {
+      return ( <div><p>Please Add Income and savings in the profile tab</p></div>)
+    }
+
+    const income = total.data[0].income;
+
+    const savingsGoal = total.data[0].savingsGoal;
+
+    const remainingFunds = income - savingsGoal - totalBill - totalSpending;
 
     return (
       <div className="center-align">
-        {console.log('props: ', this.props)}
         <BudgetRings billData={this.props.bills}/>
-        {console.log(totalBill)}
-        <IncomeComponent totalBill={totalBill}/>
+        <IncomeComponent
+          totalBill={totalBill}
+          totalSpending={totalSpending}
+          totalSaving={totalSaving}
+          income={income}
+          savingsGoal ={savingsGoal}
+          remainingFunds={remainingFunds}
+        />
         <BudgetContainer />
       </div>
     );
@@ -54,7 +90,10 @@ class Dashboard extends Component {
 
 function mapStateToProps(state) {
   return {
-    bills: state.bills
+    bills: state.bills,
+    spendings: state.spendings,
+    savings: state.savings,
+    total: state.savingIncome
   }
 }
 
--- client/src/components/budget/IncomeComponent.js
+++ client/src/components/budget/IncomeComponent.js
@@ -94,14 +94,14 @@ class IncomeComponent extends Component {
     return (
       <div style={IncomeComponentStyle} className='row'>
         <div className="col s6 left-align">
-          <p><strong>Total Earning --- </strong>  </p>
-          <p><strong>Savings Goal --- </strong>   </p>
-          <p><strong>Total Savings --- </strong> </p>
+          <p><strong>Total Earning --- </strong> {this.props.income} </p>
+          <p><strong>Savings Goal --- </strong> {this.props.savingsGoal} </p>
+          <p><strong>Total Savings --- </strong> {this.props.totalSaving}</p>
         </div>
         <div className="col s6 left-align">
-          <p><strong>Total Spendings --- </strong>  </p>
+          <p><strong>Total Spendings --- </strong> {this.props.totalSpending} </p>
           <p><strong>Total Bills --- </strong> {this.props.totalBill} </p>
-          <p><strong>Remaining Funds --- </strong>  </p>
+          <p><strong>Remaining Funds --- </strong> {this.props.remainingFunds} </p>
         </div>
       </div>
     );
